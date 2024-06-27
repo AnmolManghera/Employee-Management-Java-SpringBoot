fc8ca2aa4419387f126487bd5bf83cc8f0ee1998
AnmolManghera <anmoldeepmanghera@gmail.com>
2024-06-27 14:06:31 +0530

    first commit

---
+++ backend/src/main/java/net/neuedaproject/backend/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package net.neuedaproject.backend.mapper;
+
+import net.neuedaproject.backend.dto.EmployeeDto;
+import net.neuedaproject.backend.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
